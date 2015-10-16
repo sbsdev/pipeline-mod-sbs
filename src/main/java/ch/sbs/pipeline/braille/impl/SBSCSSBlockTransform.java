7cc05e4d00412ffd2b90d9513e992c5821e9d771
Bert Frees <bertfrees@gmail.com>
2015-10-16 10:27:55 +0200

    Base on mod-braille 1.9.6

--- src/main/java/ch/sbs/pipeline/braille/impl/SBSCSSBlockTransform.java
+++ src/main/java/ch/sbs/pipeline/braille/impl/SBSCSSBlockTransform.java
@@ -5,13 +5,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.net.URI;
 import javax.xml.namespace.QName;
 
-import static com.google.common.base.Objects.toStringHelper;
-import static com.google.common.collect.Iterables.concat;
-import static com.google.common.collect.Iterables.transform;
+import com.google.common.base.Objects;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableMap;
 
@@ -19,15 +16,19 @@ import static org.daisy.pipeline.braille.css.Query.parseQuery;
 import static org.daisy.pipeline.braille.common.util.Files.unpack;
 import static org.daisy.pipeline.braille.common.util.Tuple3;
 import static org.daisy.pipeline.braille.common.util.URIs.asURI;
+import org.daisy.pipeline.braille.common.AbstractTransform;
+import org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.Function;
+import org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.Iterables;
+import static org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.Iterables.concat;
+import static org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.Iterables.transform;
+import static org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.logCreate;
+import static org.daisy.pipeline.braille.common.AbstractTransform.Provider.util.logSelect;
 import org.daisy.pipeline.braille.common.CSSBlockTransform;
+import org.daisy.pipeline.braille.common.TextTransform;
 import org.daisy.pipeline.braille.common.Transform;
-import org.daisy.pipeline.braille.common.Transform.AbstractTransform;
 import static org.daisy.pipeline.braille.common.Transform.Provider.util.dispatch;
-import static org.daisy.pipeline.braille.common.Transform.Provider.util.logCreate;
-import static org.daisy.pipeline.braille.common.Transform.Provider.util.logSelect;
 import static org.daisy.pipeline.braille.common.Transform.Provider.util.memoize;
 import static org.daisy.pipeline.braille.common.util.Locales.parseLocale;
-import org.daisy.pipeline.braille.common.WithSideEffect;
 import org.daisy.pipeline.braille.common.XProcTransform;
 import org.daisy.pipeline.braille.libhyphen.LibhyphenHyphenator;
 import org.daisy.pipeline.braille.liblouis.LiblouisTranslator;
@@ -39,8 +40,6 @@ import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 import org.osgi.service.component.ComponentContext;
 
-import org.slf4j.Logger;
-
 public interface SBSCSSBlockTransform extends CSSBlockTransform, XProcTransform {
 	
 	@Component(
@@ -50,7 +49,8 @@ public interface SBSCSSBlockTransform extends CSSBlockTransform, XProcTransform 
 			CSSBlockTransform.Provider.class
 		}
 	)
-	public class Provider implements XProcTransform.Provider<SBSCSSBlockTransform>, CSSBlockTransform.Provider<SBSCSSBlockTransform> {
+	public class Provider extends AbstractTransform.Provider<SBSCSSBlockTransform>
+		                  implements XProcTransform.Provider<SBSCSSBlockTransform>, CSSBlockTransform.Provider<SBSCSSBlockTransform> {
 		
 		private URI href;
 		private URI displayTable;
@@ -62,6 +62,22 @@ public interface SBSCSSBlockTransform extends CSSBlockTransform, XProcTransform 
 			unpack(context.getBundleContext().getBundle().getEntry("/liblouis/virtual.dis"), f);
 			displayTable = asURI(f);
 		}
+			
+		private final static String grade0Table = "http://www.sbs.ch/pipeline/liblouis/tables/" +
+			"sbs.dis,sbs-de-core6.cti,sbs-de-accents.cti,sbs-special.cti,sbs-whitespace.mod,sbs-numsign.mod," +
+			"sbs-litdigit-upper.mod,sbs-de-core.mod,sbs-de-g0-core.mod,sbs-de-hyph-none.mod,sbs-de-accents-ch.mod," +
+			"sbs-special.mod";
+		private final static String grade1Table = "http://www.sbs.ch/pipeline/liblouis/tables/" +
+			"sbs.dis,sbs-de-core6.cti,sbs-de-accents.cti,sbs-special.cti,sbs-whitespace.mod,sbs-numsign.mod," +
+			"sbs-litdigit-upper.mod,sbs-de-core.mod,sbs-de-g0-core.mod,sbs-de-g1-white.mod,sbs-de-g1-core.mod," +
+			"sbs-de-hyph-none.mod,sbs-de-accents-ch.mod,sbs-special.mod";
+		private final static String grade2Table = "http://www.sbs.ch/pipeline/liblouis/tables/" +
+			"sbs.dis,sbs-de-core6.cti,sbs-de-accents.cti,sbs-special.cti,sbs-whitespace.mod,sbs-de-letsign.mod," +
+			"sbs-numsign.mod,sbs-litdigit-upper.mod,sbs-de-core.mod,sbs-de-g2-white.mod,sbs-de-g2-core.mod," +
+			"sbs-de-hyph-none.mod,sbs-de-accents-ch.mod,sbs-special.mod";
+		private final static String hyphenationTable = "(libhyphen-table:'http://www.sbs.ch/pipeline/hyphen/hyph_de_DE.dic')";
+		
+		private final static Iterable<SBSCSSBlockTransform> empty = Iterables.<SBSCSSBlockTransform>empty();
 		
 		/**
 		 * Recognized features:
@@ -71,103 +87,66 @@ public interface SBSCSSBlockTransform extends CSSBlockTransform, XProcTransform 
 		 * - grade: `0', `1' or `2'.
 		 *
 		 */
-		public Iterable<SBSCSSBlockTransform> get(String query) {
-			 return impl.get(query);
-		 }
-	
-		public Transform.Provider<SBSCSSBlockTransform> withContext(Logger context) {
-			return impl.withContext(context);
-		}
-	
-		private Transform.Provider.MemoizingProvider<SBSCSSBlockTransform> impl = new ProviderImpl(null);
-	
-		private class ProviderImpl extends AbstractProvider<SBSCSSBlockTransform> {
-			
-			private final static String grade0Table = "http://www.sbs.ch/pipeline/liblouis/tables/" +
-				"sbs.dis,sbs-de-core6.cti,sbs-de-accents.cti,sbs-special.cti,sbs-whitespace.mod,sbs-numsign.mod," +
-				"sbs-litdigit-upper.mod,sbs-de-core.mod,sbs-de-g0-core.mod,sbs-de-hyph-none.mod,sbs-de-accents-ch.mod," +
-				"sbs-special.mod";
-			private final static String grade1Table = "http://www.sbs.ch/pipeline/liblouis/tables/" +
-				"sbs.dis,sbs-de-core6.cti,sbs-de-accents.cti,sbs-special.cti,sbs-whitespace.mod,sbs-numsign.mod," +
-				"sbs-litdigit-upper.mod,sbs-de-core.mod,sbs-de-g0-core.mod,sbs-de-g1-white.mod,sbs-de-g1-core.mod," +
-				"sbs-de-hyph-none.mod,sbs-de-accents-ch.mod,sbs-special.mod";
-			private final static String grade2Table = "http://www.sbs.ch/pipeline/liblouis/tables/" +
-				"sbs.dis,sbs-de-core6.cti,sbs-de-accents.cti,sbs-special.cti,sbs-whitespace.mod,sbs-de-letsign.mod," +
-				"sbs-numsign.mod,sbs-litdigit-upper.mod,sbs-de-core.mod,sbs-de-g2-white.mod,sbs-de-g2-core.mod," +
-				"sbs-de-hyph-none.mod,sbs-de-accents-ch.mod,sbs-special.mod";
-			private final static String hyphenationTable = "(libhyphen-table:'http://www.sbs.ch/pipeline/hyphen/hyph_de_DE.dic')";
-		
-			private ProviderImpl(Logger context) {
-				super(context);
-			}
-		
-			protected Transform.Provider.MemoizingProvider<SBSCSSBlockTransform> _withContext(Logger context) {
-				return new ProviderImpl(context);
-			}
-		
-			protected final Iterable<WithSideEffect<SBSCSSBlockTransform,Logger>> __get(String query) {
-				Map<String,Optional<String>> q = new HashMap<String,Optional<String>>(parseQuery(query));
-				Optional<String> o;
-				if ((o = q.remove("locale")) != null)
-					if (!"de".equals(parseLocale(o.get()).getLanguage()))
-						return empty;
-				if ((o = q.remove("translator")) != null)
-					if (o.get().equals("sbs"))
-						if ((o = q.remove("grade")) != null) {
-							final int grade;
-							if (o.get().equals("0"))
-								grade = 0;
-							else if (o.get().equals("1"))
-								grade = 1;
-							else if (o.get().equals("2"))
-								grade = 2;
-							else
-								return empty;
-							if (q.size() == 0) {
-								Iterable<WithSideEffect<LibhyphenHyphenator,Logger>> hyphenators
-									= logSelect(hyphenationTable, libhyphenHyphenatorProvider.get(hyphenationTable));
-								final String liblouisTable = "(liblouis-table:'" + displayTable
-								        + "," + ( grade == 2 ? grade2Table : ( grade == 1 ? grade1Table : grade0Table )) + "')";
-								return transform(
+		protected final Iterable<SBSCSSBlockTransform> _get(String query) {
+			Map<String,Optional<String>> q = new HashMap<String,Optional<String>>(parseQuery(query));
+			Optional<String> o;
+			if ((o = q.remove("locale")) != null)
+				if (!"de".equals(parseLocale(o.get()).getLanguage()))
+					return empty;
+			if ((o = q.remove("translator")) != null)
+				if (o.get().equals("sbs"))
+					if ((o = q.remove("grade")) != null) {
+						final int grade;
+						if (o.get().equals("0"))
+							grade = 0;
+						else if (o.get().equals("1"))
+							grade = 1;
+						else if (o.get().equals("2"))
+							grade = 2;
+						else
+							return empty;
+						if (q.size() == 0) {
+							Iterable<LibhyphenHyphenator> hyphenators = logSelect(hyphenationTable, libhyphenHyphenatorProvider);
+							final String liblouisTable = "(liblouis-table:'" + displayTable
+								+ "," + ( grade == 2 ? grade2Table : ( grade == 1 ? grade1Table : grade0Table )) + "')";
+							return concat(
+								transform(
 									hyphenators,
-									new WithSideEffect.Function<LibhyphenHyphenator,SBSCSSBlockTransform,Logger>() {
-										public SBSCSSBlockTransform _apply(LibhyphenHyphenator h) {
-											String translatorQuery = liblouisTable + "(hyphenator:" + h.getIdentifier() + ")";
-											try {
-												applyWithSideEffect(
-													logSelect(
-														translatorQuery,
-														liblouisTranslatorProvider.get(translatorQuery)).iterator().next()); }
-											catch (NoSuchElementException e) {
-												throw new NoSuchElementException(); }
-											return applyWithSideEffect(
-												logCreate(
-													(SBSCSSBlockTransform)new TransformImpl(grade, translatorQuery))); }}); }}
-				return empty;
-			}
+									new Function<LibhyphenHyphenator,Iterable<SBSCSSBlockTransform>>() {
+										public Iterable<SBSCSSBlockTransform> _apply(LibhyphenHyphenator h) {
+											final String translatorQuery = liblouisTable + "(hyphenator:" + h.getIdentifier() + ")";
+											return transform(
+												logSelect(translatorQuery, liblouisTranslatorProvider),
+												new Function<LiblouisTranslator,SBSCSSBlockTransform>() {
+													public SBSCSSBlockTransform _apply(LiblouisTranslator translator) {
+														return __apply(logCreate(new TransformImpl(grade, translatorQuery, translator))); }}); }})); }}
+			return empty;
 		}
-	
-		private final static Iterable<WithSideEffect<SBSCSSBlockTransform,Logger>> empty
-		= Optional.<WithSideEffect<SBSCSSBlockTransform,Logger>>absent().asSet();
 		
 		private class TransformImpl extends AbstractTransform implements SBSCSSBlockTransform {
 			
 			private final Tuple3<URI,QName,Map<String,String>> xproc;
 			private final int grade;
+			private final LiblouisTranslator translator;
 			
-			private TransformImpl(int grade, String translatorQuery) {
+			private TransformImpl(int grade, String translatorQuery, LiblouisTranslator translator) {
 				Map<String,String> options = ImmutableMap.of("query", translatorQuery);
 				xproc = new Tuple3<URI,QName,Map<String,String>>(href, null, options);
 				this.grade = grade;
+				this.translator = translator;
 			}
-	
+			
+			public TextTransform asTextTransform() {
+				return translator;
+			}
+			
 			public Tuple3<URI,QName,Map<String,String>> asXProc() {
 				return xproc;
 			}
-	
+			
 			@Override
 			public String toString() {
-				return toStringHelper(SBSCSSBlockTransform.class.getSimpleName())
+				return Objects.toStringHelper(SBSCSSBlockTransform.class.getSimpleName())
 					.add("grade", grade)
 					.toString();
 			}
