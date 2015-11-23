bb4f44dffc09f79870bd25f8bde26ca149d1a70b
Bert Frees <bertfrees@gmail.com>
2015-11-23 13:09:13 +0100

    Release notes for v1.2.0

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil
+  . ((compile-command . (format "cd %s && mvn clean install"
+                                (locate-dominating-file buffer-file-name "pom.xml")))))
+ ("NEWS.md"
+  . ((nil . ((fill-column . 100))))))
